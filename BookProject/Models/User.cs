1fd8d17de03c20853ef0ba78ef375faad22ae953
unknown <tanishaj1503@gmail.com>
2024-05-28 15:58:13 +0530

    first comit

---
+++ BookProject/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace BookProject.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
---
+++ BookProject/Models/UserRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookProject.Models
+{
+    public class UserRoleDto
+    {
+        public string Username { get; set; }
+        public string RoleName { get; set; }
+    }
+}
